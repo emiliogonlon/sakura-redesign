c229bf91c331acce184fdf3067fc0b2b66d99a8a
Emilio <emiliogonlon@outlook.com>
2023-06-01 19:14:45 +0200

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://emiliogonlon.github.io',
+    base: '/sakura-redesign',
+  })
