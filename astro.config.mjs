ed482938b26dc4fd802d7e6a01442a19d3e8d7e6
Magnus Bolton <magnus@bolton.dk>
2023-12-14 13:34:10 +0100

    ny commit noget med js i logo er galt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
 
 export default defineConfig({
-    site: 'https://emiliogonlon.github.io',
-    base: '/sakura-redesign',
-  })
+  site: "https://emiliogonlon.github.io",
+  base: "/sakura-redesign",
+});
